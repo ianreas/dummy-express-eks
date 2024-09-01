49ef4de92dc1145ac8f4b888749f633556c326b6
ianreas <muhammedik111101@gmail.com>
2024-09-01 17:53:31 -0400

    added meow and bark

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+app.get("/meow", (req, res) => {
+  console.log("meow stdout");
+  console.error("meow stderr");
+  res.json({ message: "Meow from Express.js on EKS!" });
+});
+
+app.get("/bark", (req, res) => {
+  console.log("bark stdout");
+  console.error("bark stderr");
+  res.json({ message: "Bark from Express.js on EKS!" });
+});
